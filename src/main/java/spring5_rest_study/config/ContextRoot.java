440768bb2ab5e97a561b1cb5e72d99df57563931
jylee <stpn94@gmail.com>
2021-05-28 09:44:20 +0900

    contextDataSource contextSqlSession 작업 및 TEST 완료

--- src/main/java/spring5_rest_study/config/ContextRoot.java
+++ src/main/java/spring5_rest_study/config/ContextRoot.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ContextDataSource.class})
+@Import({ContextDataSource.class,ContextSqlSession.class})
 @ComponentScan(basePackages = {})
 public class ContextRoot {
 
