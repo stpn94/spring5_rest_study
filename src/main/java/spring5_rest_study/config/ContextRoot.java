4889ba6e58db9ff832c749383d033810587043c8
jylee <stpn94@gmail.com>
2021-05-28 09:38:59 +0900

    rest Api study 초기작업 2

---
+++ src/main/java/spring5_rest_study/config/ContextRoot.java
@@ -0,0 +1,12 @@
+package spring5_rest_study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ContextDataSource.class})
+@ComponentScan(basePackages = {})
+public class ContextRoot {
+
+}
