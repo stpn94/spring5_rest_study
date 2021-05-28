bdfd97a9cb115f2bcefe43439a348930d3f158ef
jylee <stpn94@gmail.com>
2021-05-28 10:11:33 +0900

    초기작업 완료

---
+++ src/main/java/spring5_rest_study/mappers/MemberMapper.java
@@ -0,0 +1,22 @@
+package spring5_rest_study.mappers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import spring5_rest_study.dto.Member;
+
+@Component
+public interface MemberMapper {
+
+	Member selectMemberById(long memId);
+
+	List<Member> selectMemberAll();
+
+	int insertMember(Member member);
+
+	int updateMember(Member member);
+
+	int deleteMember(Member member);
+
+}
---
+++ src/test/java/spring5_rest_study/mappers/MemberMapperTest.java
@@ -0,0 +1,85 @@
+package spring5_rest_study.mappers;
+
+import java.util.List;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import spring5_rest_study.config.ContextRoot;
+import spring5_rest_study.dto.Member;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { ContextRoot.class })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@WebAppConfiguration
+public class MemberMapperTest {
+	protected static final Log log = LogFactory.getLog(MemberMapperTest.class);
+
+	@Autowired
+	private MemberMapper mapper;
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println();
+	}
+
+	
+
+	@Test
+	public void test1InsertMember() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Member newMember = new Member("test20@test.co.kr", "1111", "test20");
+
+		int res = mapper.insertMember(newMember);
+		Assert.assertEquals(1, res);
+	}
+	
+	@Test
+	public void test2SelectMemberAll() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		List<Member> list = mapper.selectMemberAll();
+		Assert.assertNotNull(list);
+
+		list.forEach(s -> log.debug(s.toString()));
+	}
+
+	@Test
+	public void test3SelectMemberById() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+
+		Member member = mapper.selectMemberById(3);
+		Assert.assertNotNull(member);
+
+		log.debug(member.toString());
+	}
+
+	@Test
+	public void test4UpdateMember() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+
+		Member newMember = new Member("test20@test.co.kr", "2222", "테스트20");
+
+		int res = mapper.updateMember(newMember);
+		Assert.assertEquals(1, res);
+	}
+
+	@Test
+	public void test5DeleteMember() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+
+		Member newMember = new Member("test20@test.co.kr", "2222", "테스트20");
+		int res = mapper.deleteMember(newMember);
+		Assert.assertEquals(1, res);
+	}
+
+}
